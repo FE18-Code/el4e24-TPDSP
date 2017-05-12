62e8567486e8758a21a1d4bd1d22f69e3dc64715
ams <ams.gh@hmamail.com>
2017-05-12 14:33:55 +0200

    led toggle @100ms
    tested, working

--- Labo1.c
+++ Labo1.c
@@ -10,6 +10,7 @@
 #include "DSP2833x_Device.h"
 
 void InitSysCtrl(void);
+void wait100ms(void);
 void Gpio_select(void);
 
 
@@ -18,28 +19,41 @@ void Gpio_select(void);
 //###########################################################################
 void main(void){
 	
-	
+	InitSysCtrl();
+	Gpio_select();
 
-
-
+	while(1){
+		GpioDataRegs.GPBTOGGLE.bit.GPIO32 = 1 ;
+		wait100ms();
+	}
 }
 
+void wait100ms(){
+	long i;
 
+	for(i=0;i<577000;i++){
+		asm (" NOP ");
+		asm (" NOP ");
+		EALLOW;
+		SysCtrlRegs.WDKEY = 0xAA;
+		EDIS;
+	}
+}
 
 void Gpio_select(void){
 	EALLOW;
-	GpioCtrlRegs.GPAMUX1.all = ?;		// GPIO15 ... GPIO0 = General Puropse I/O
-	GpioCtrlRegs.GPAMUX2.all = ?;		// GPIO31 ... GPIO16 = General Purpose I/O
-	GpioCtrlRegs.GPBMUX1.all = ?;		// GPIO47 ... GPIO32 = General Purpose I/O
-	GpioCtrlRegs.GPBMUX2.all = ?;		// GPIO63 ... GPIO48 = General Purpose I/O
-	GpioCtrlRegs.GPCMUX1.all = ?;		// GPIO79 ... GPIO64 = General Purpose I/O
-	GpioCtrlRegs.GPCMUX2.all = ?;		// GPIO87 ... GPIO80 = General Purpose I/O
+	GpioCtrlRegs.GPAMUX1.all = 00;		// GPIO15 ... GPIO0 = General Puropse I/O
+	GpioCtrlRegs.GPAMUX2.all = 00;		// GPIO31 ... GPIO16 = General Purpose I/O
+	GpioCtrlRegs.GPBMUX1.all = 00;		// GPIO47 ... GPIO32 = General Purpose I/O
+	GpioCtrlRegs.GPBMUX2.all = 00;		// GPIO63 ... GPIO48 = General Purpose I/O
+	GpioCtrlRegs.GPCMUX1.all = 00;		// GPIO79 ... GPIO64 = General Purpose I/O
+	GpioCtrlRegs.GPCMUX2.all = 00;		// GPIO87 ... GPIO80 = General Purpose I/O
 
-	GpioCtrlRegs.GPADIR.all = ?;
+	GpioCtrlRegs.GPADIR.all = 0;
 
-	GpioCtrlRegs.GPBDIR.all = ?;		// 
-	GpioCtrlRegs.GPBDIR.bit.GPIO32 = ?;	//  LED LD3 at GPIO32
-	GpioCtrlRegs.GPCDIR.all = ?;		// 
+	GpioCtrlRegs.GPBDIR.all = 0;		//
+	GpioCtrlRegs.GPBDIR.bit.GPIO32 = 1;	//  LED LD3 at GPIO32
+	GpioCtrlRegs.GPCDIR.all = 0;		//
 	EDIS;
 }   
 
